01d2c5e7db0bcad3640c507669d6373f44d55cb9
Chloe-Jane Monochrome <131207798+chloechrome@users.noreply.github.com>
2025-11-10 20:42:27 +0000

    something wicked this way comes
    why did index.html revert to an older version i hate you

--- sct/bar.js
+++ sct/bar.js
@@ -6,9 +6,10 @@ function bar() {
 											<a class='link' href='../'>Updates</a><br>
 										</div>
 										<div class='internav' id='content'><b style='font-size:18;'>Content</b><br><br>
+											<a class='link' href='../chars.html'>Characters</a><br>
 											<a class='link' href='../events.html'>Events</a><br>
 											<a class='link' href='../factions.html'>Factions</a><br>
-											<a class='link' href='../locations.html'>Locations</a><br>
+											<a class='link' href='../locations.html'>Locations</a><br>----------------<br>
 											<a class='link' href='../themes.html'>Themes</a><br>
 											<a class='link' href='../units.html'>Units</a><br></div>
 										<div class='internav' id=''><b style='font-size:18;'>Discord</b><br><br>
