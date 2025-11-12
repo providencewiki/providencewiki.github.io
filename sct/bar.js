aacf54e7d8c7ec063f4b969a00f476ea1d6d0eb9
Chloe-Jane Monochrome <131207798+chloechrome@users.noreply.github.com>
2025-11-12 21:56:21 +0000

    wiki update, 12/11/2025. dump all files from local.

--- sct/bar.js
+++ sct/bar.js
@@ -9,7 +9,8 @@ function bar() {
 											<a class='link' href='../chars.html'>Characters</a><br>
 											<a class='link' href='../events.html'>Events</a><br>
 											<a class='link' href='../factions.html'>Factions</a><br>
-											<a class='link' href='../locations.html'>Locations</a><br>----------------<br>
+											<a class='link' href='../locations.html'>Locations</a><br>
+											<a class='link' href=',,/misc.html'>Other</a>----------------<br>
 											<a class='link' href='../themes.html'>Themes</a><br>
 											<a class='link' href='../units.html'>Units</a><br></div>
 										<div class='internav' id=''><b style='font-size:18;'>Discord</b><br><br>
